f02bb9b88c570f0846b7405f982c0daab540c59b
middled <saso.ferk@gmail.com>
2015-09-12 08:48:26 +0200

    base

--- app/src/main/java/com/example/sao/helloworld/MainActivity.java
+++ app/src/main/java/com/example/sao/helloworld/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    /*@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -33,5 +33,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
